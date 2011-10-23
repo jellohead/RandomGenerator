eb6306c3017a13c049832f08a0782fa91f012326
Pat Escalona <jellohead7000@gmail.com>
2011-10-23 10:26:05 -0700

    Initial Commit

---
+++ RandomGenerator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RandomGenerator
+//
+//  Created by Pat Escalona on 10/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RandomGenerator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RandomGenerator
+//
+//  Created by Pat Escalona on 10/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
